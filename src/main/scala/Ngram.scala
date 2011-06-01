a2147ac327d63861570b0e862960e4ed5cf34061
Evan Meagher <evan.meagher@gmail.com>
2011-05-31 19:46:04 -0700

    Manage completion word count internal to Ngram class and s/words/leading_words/.

--- src/main/scala/Ngram.scala
+++ src/main/scala/Ngram.scala
@@ -3,15 +3,17 @@ package com.evnm.markovprocessor
 /*
  * An Ngram class.
  */
-class Ngram(val words: List[String], val count: Int, val choices: Map[String, Int]) {
+class Ngram(val leading_words: List[String], val choices: Map[String, Int]) {
+  val count = choices.foldLeft(0) { (acc, kv) => acc + kv._2 }
+
   /*
    * Returns a copy of the Ngram with the argument string added to the list of
    * completions.
    */
   def +(str: String): Ngram = {
     choices get str match {
-      case Some(i) => new Ngram(words, count + 1, choices + (str -> (i + 1)))
-      case None => new Ngram(words, count + 1, choices + (str -> 1))
+      case Some(i) => new Ngram(leading_words, choices + (str -> (i + 1)))
+      case None => new Ngram(leading_words, choices + (str -> 1))
     }
   }
 }
