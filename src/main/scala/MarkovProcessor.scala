9a38cd0c4f0148824de6ee25e89c0b9a43d93eed
Evan Meagher <evan.meagher@gmail.com>
2011-07-06 22:19:19 -0700

    Add MarkovProcessor.buildTree and -.buildList, plus associated tests.

--- src/main/scala/MarkovProcessor.scala
+++ src/main/scala/MarkovProcessor.scala
@@ -24,4 +24,51 @@ object MarkovProcessor {
     val completions = lst drop (n - 1)
     leading_words zip completions
   }
+
+
+  /*
+   * Build an Ngram tree from a given string.
+   */
+  def buildTree(n: Int, text: String): BinaryTree[Ngram] =
+    Node.insertList(groupWords(n, text.split("( | \n)+").toList))
+
+
+  /*
+   * Finds a list of leading words within an Ngram tree. Returns an Ngram option.
+   */
+  def find(words: List[String], tree: BinaryTree[Ngram]): Option[Ngram] = {
+    tree match {
+      case EmptyNode => None
+      case Node(ngram @ Ngram(leading, compls), left, right) => {
+        Node.stringListCompare(words, leading) match {
+          case -1 => find(words, left)
+          case  0 => Some(ngram)
+          case  1 => find(words, right)
+        }
+      }
+    }
+  }
+
+
+  /*
+   * Returns a list of approximately count words that are randomly generated using the
+   * given Ngram tree.
+   */
+  def buildList(count: Int, tree: BinaryTree[Ngram]): List[String] = {
+    tree match {
+      case Node(ngram, left, right) => {
+        def shift(lst: List[String], n: Int): List[String] = {
+          val ngram = find(lst.tail, tree)
+          if (!ngram.isDefined || n == 0) lst.init
+          else lst.head :: shift(ngram.get.leading_words ::: List(ngram.get.randomWord), n - 1)
+        }
+
+        val initial_words = Node.pickRandom(tree)
+        shift(initial_words, count - initial_words.length)
+      }
+      case _ => Nil
+    }
+  }
+
+
 }
--- src/test/scala/MarkovProcessorSuite.scala
+++ src/test/scala/MarkovProcessorSuite.scala
@@ -1,9 +1,13 @@
 package com.evnm.markovprocessor
 
+import scala.io.Source
 import org.scalatest.FunSuite
 import com.evnm.markovprocessor.{MarkovProcessor => MP}
 
 class MarkovProcessorSuite extends FunSuite {
+  /*
+   * Test groupWords.
+   */
   test("groupWords should return Nil when input list is Nil") {
     assert(MP.groupWords(0, Nil) == Nil)
     assert(MP.groupWords(3, Nil) == Nil)
@@ -18,5 +22,74 @@ class MarkovProcessorSuite extends FunSuite {
   test("groupWords should return appropriate list on valid input") {
     val lst = List((List("a", "b"), "c"), (List("b", "c"), "d"), (List("c", "d"), "e"))
     assert(MP.groupWords(3, List("a", "b", "c", "d", "e")) == lst)
+  }
+
+  test("Grouping massive lists should not generate stack overflows") {
+    val words = Source.fromFile("src/test/resources/hamlet.txt").mkString.split("( |\n)+").toList
+    MP.groupWords(4, words)
+  }
+
+  /*
+   * Test buildTree.
+   */
+  test("buildTree should return an EmptyNode when given an empty string") {
+    MP.buildTree(3, "") match {
+      case EmptyNode => assert(true)
+      case _ => assert(false, "tree built from empty string should be an EmptyNode")
+    }
+  }
+
+  test("buildTree should return a single leaf Node when given a string of n=3 words") {
+    MP.buildTree(3, "oh hai there") match {
+      case Node(Ngram(lw, c), EmptyNode, EmptyNode) => {
+        assert(lw == List("oh", "hai"))
+        assert(c == Map("there" -> 1))
+      }
+      case _ => assert(false, "tree built was invalid")
+    }
+  }
+
+  test("buildTree should return an appropriate tree when given a non-trivial string") {
+    MP.buildTree(4, "four score and seven years ago") match {
+      case Node(Ngram(lw, c), left, right) => {
+        assert(lw == List("four", "score", "and"))
+        assert(c == Map("seven" -> 1))
+        right match {
+          case Node(Ngram(lw, c), left, EmptyNode) => {
+            assert(lw == List("score", "and", "seven"))
+            assert(c == Map("years" -> 1))
+          }
+          case _ => assert(false, "right should not be empty")
+        }
+        left match {
+          case Node(Ngram(lw, c), l, r) => {
+            assert(lw == List("and", "seven", "years"))
+            assert(c == Map("ago" -> 1))
+          }
+          case _ => assert(false, "left should not be empty")
+        }
+      }
+      case _ => assert(false, "tree built was invalid")
+    }
+  }
+
+  /*
+   * Test find.
+   */
+  test("find should return None on an empty tree") {
+    assert(!MP.find(List(), EmptyNode).isDefined)
+  }
+
+  test("find should return a valid ngram option on a non-empty tree") {
+    val result =
+      MP.find(List("foo"), Node(Ngram(List("foo"),Map("bar" -> 1)), EmptyNode, EmptyNode))
+    assert(result.isDefined)
+    assert(result.get.leading_words == List("foo"))
+    assert(result.get.choices == Map("bar" -> 1))
+  }
+
+  test("blah") {
+    val words = Source.fromFile("src/test/resources/hamlet.txt").mkString
+    println(MP.buildList(100, MP.buildTree(4, words)).mkString(" "))
   }
 }
